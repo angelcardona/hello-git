8ec470cadc7b1271d64ab24b33ac226d65808e35
angelcardona <miguel.acardona98@gmail.com>
2025-04-20 09:30:41 -0500

    Created a division function

---
+++ hellogit.py
@@ -0,0 +1,9 @@
+def product():
+    x=int(input("Please introduce a number"))
+    y=int(input("Please introduce a number"))
+    if y != 0:
+        c=x/y
+        return c
+    else :
+        return print("Division by o doesn't exist")
+    print("The result is {c} ")
